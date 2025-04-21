5064622da84e44d93c730814006af04c8a39dd9b
vald1113 <vladik2006jr@gmail.com>
2025-04-21 11:55:12 +0200

    initial commit

---
+++ validations.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+  body("email", "Invalid email format").isEmail(),
+  body("password", "Password must be at least 5 characters long").isLength({
+    min: 5,
+  }),
+];
+
+export const registerValidation = [
+  body("email", "Invalid email format").isEmail(),
+  body("password", "Password must be at least 5 characters long").isLength({
+    min: 5,
+  }),
+  body("fullName", "Please provide your name").isLength({ min: 3 }),
+  body("avatarUrl", "Invalid avatar URL").optional().isURL(),
+];
+
+export const postCreateValidation = [
+  body("title", "Enter a post title").isLength({ min: 3 }).isString(),
+  body("text", "Enter the post content").isLength({ min: 3 }).isString(),
+  body("tags", "Invalid tags format").optional().isString(),
+  body("imageUrl", "Invalid image URL").optional().isString(),
+];
